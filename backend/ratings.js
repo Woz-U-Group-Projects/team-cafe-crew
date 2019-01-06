c8d45d71d3c223b1e169bb599779f1b23b402a24
skmeyer2018 <skmeyer2014@gmail.com>
2019-01-05 20:24:41 -0700

    search recipes by name, user, category and instructions

---
+++ backend/ratings.js
@@ -0,0 +1,20 @@
+// /backend/ratings.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//var MongoClient = require('mongodb').MongoClient;
+//const Schema=MongoClient.Schema
+// this will be our data base's data structure 
+const RatingsSchema = new Schema(
+  {
+    recipeId: Number,
+    recipeUser: String,
+    recipeName: String,
+    recipeRating: Number
+
+  },
+  { timestamps: true }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Ratings", RatingsSchema);
+//module.exports=MongoClient.model("Data", DataSchema );
