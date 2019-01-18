5773c2f265a22284b5ae9dbb4cdcbc71c15a76aa
Jason Orosco <31417349+jorosco55@users.noreply.github.com>
2019-01-18 09:27:51 -0700

    Updated to connect to MLabs
    connected Spring to MLabs and took out the Mongo embed

--- VirtualChef/VritualCafe/src/main/java/com/virtual/cafe/MongoConfig.java
+++ VirtualChef/VritualCafe/src/main/java/com/virtual/cafe/MongoConfig.java
@@ -1,28 +1,18 @@
 package com.virtual.cafe;
 
-import java.io.IOException;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
-import com.mongodb.MongoClient;
-
-import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
-
 //Comment out to use MLabs
-@Configuration
+//@Configuration
 public class MongoConfig {
  
     private static final String MONGO_DB_URL = "localhost";
     private static final String MONGO_DB_NAME = "recipe";
     //Comment out to use MLabs
-    @Bean
-    public MongoTemplate mongoTemplate() throws IOException {
-        EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
-        mongo.setBindIp(MONGO_DB_URL);
-        MongoClient mongoClient = mongo.getObject();
-        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
-        return mongoTemplate;
-    }
+    //@Bean
+    //public MongoTemplate mongoTemplate() throws IOException {
+        //EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
+        //mongo.setBindIp(MONGO_DB_URL);
+        //MongoClient mongoClient = mongo.getObject();
+        //MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
+        //return mongoTemplate;
+    //}
 }
